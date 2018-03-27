5f58283e7775ad0fc995eed017f72368118754da
aohanlon541 <allison.t.ohanlon@gmail.com>
2018-03-27 11:15:54 -0500

    updated

--- public/js/index.js
+++ public/js/index.js
@@ -4,12 +4,18 @@
 	    if ($(window).width() > 700) {
 	        skrollr.init();
 	    }
+	
+	$(".example").on("hover", () => {
+		
+	})
+
 
 	    // disable skrollr if the window is resized below 768px wide
-	    $(window).on('resize', function() {
+	    $(window).on('resize', () => {
 	        if ($(window).width() <= 700) {
 	            skrollr.init().destroy(); // skrollr.init() returns the singleton created above
 	        }
 	    });
 	});
+
 
